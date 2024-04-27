024cbfeefd7860dbf37daf46f76a6fe6e5abaa4d
Johan Wagenheim <dospuntos@users.noreply.github.com>
2024-04-27 07:35:54 -0400

    Add file open dialog (#17)

---
+++ source/RefFilters.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2003, Heikki Suhonen
+ * Copyright 2009, Karsten Heimrich
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ * 		Heikki Suhonen <heikki.suhonen@gmail.com>
+ * 		Karsten Heimrich <host.haiku@gmx.de>
+ *
+ */
+
+#include "RefFilters.h"
+
+
+#include <Entry.h>
+#include <NodeInfo.h>
+
+
+#include <compat/sys/stat.h>
+#include <string.h>
+
+
+bool
+ImageFilter::Filter(const entry_ref* ref, BNode* node, struct stat_beos* stat, const char* fileType)
+{
+	if (S_ISDIR(stat->st_mode))
+		return true;
+
+	if (S_ISLNK(stat->st_mode)) {
+		BEntry entry(ref, true);
+		return entry.IsDirectory();
+	}
+
+	char mimeType[B_MIME_TYPE_LENGTH];
+	BNodeInfo(node).GetType(mimeType);
+	if (strncmp("image/", mimeType, 6) == 0)
+		return true;
+
+	return false;
+}
---
+++ source/RefFilters.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2003, Heikki Suhonen
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ * 		Heikki Suhonen <heikki.suhonen@gmail.com>
+ *
+ */
+#ifndef REF_FILTERS_H
+#define REF_FILTERS_H
+
+#include <FilePanel.h>
+
+
+class ImageFilter : public BRefFilter {
+public:
+			bool		Filter(const entry_ref* entryRef, BNode* node,
+							struct stat_beos* stat, const char* fileType);
+};
+
+
+#endif // REF_FILTERS_H
