ef40ea8f6bc998db977a3d7ccc2ea65e73cff449
Humdinger <humdinger@mailbox.org>
2025-03-31 17:12:27 +0200

    Remove OptionView, move into OptionWindow
    No need to put an extra BView into the BWindow.
    All functionality from OptionView was transplanted into
    OptionWindow.

--- source/OptionWindow.h
+++ source/OptionWindow.h
@@ -11,19 +11,55 @@
 #define OPTIONWINDOW_H
 
 #include "Constants.h"
-#include "OptionView.h"
+
+#include <Button.h>
+#include <CheckBox.h>
+#include <MenuField.h>
+#include <MenuItem.h>
+#include <PopUpMenu.h>
+#include <StringView.h>
+#include <TextControl.h>
+#include <TranslatorRoster.h>
 #include <Window.h>
+
 
 class OptionWindow : public BWindow {
 public:
 	int fLockW;
 	int fLockH;
 
-	OptionView* fOptionView;
-
 	OptionWindow();
 	virtual bool QuitRequested();
 	virtual void MessageReceived(BMessage* message);
+
+	BCheckBox* fSmoothBox;
+	BTextControl* fFileName;
+	BTextControl* fHeightTextbox;
+	BTextControl* fWidthTextbox;
+
+private:
+	void _ApplyEffect();
+	void _ChangeEffect(int effect);
+	void _CreateEffectPopup();
+	void _CreateFormatPopup();
+	void _SetHeight(int height);
+	void _SetWidth(int width);
+
+	int fSavedHeight;
+	int fSavedWidth;
+	int fCurrentEffect;
+
+	BCheckBox* fAspectBox;
+	// BCheckBox* Coord;
+	// BCheckBox* Grip;
+	BButton* fResetButton;
+	BButton* fUndoButton;
+	BButton* fApplyButton;
+	// BButton* fWebButton;
+	BMenuField* fFormatMenu;
+	BPopUpMenu* fFormatPopup;
+	BMenuField* fEffectMenu;
+	BPopUpMenu* fEffectPopup;
 };
 
 #endif
